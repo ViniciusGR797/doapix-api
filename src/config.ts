8c5d152ce2659065366c6cb2e6269fbd0ff6b80d
mrjonas151 <jonas.aquinos@hotmail.com>
2024-03-25 16:34:31 -0300

    Feat: trocando a porta de conexão da api para integração com o front

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 // Defina as configurações que você deseja usar
 const config = {
-  port: parseInt(process.env.PORT || '3000', 10),
+  port: parseInt(process.env.PORT || '3333', 10),
   bdUri: process.env.DB_URI || 'postgresql://username:password@hostname/database?sslmode=require',
   saltRounds: parseInt(process.env.SALT_ROUNDS || '10', 10),
   jwt: {
