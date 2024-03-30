86d5ced60232c919a99b2e6594a6439472c24217
mrjonas151 <jonas.aquinos@hotmail.com>
2024-03-30 19:36:59 -0300

    feat: Criando o cadastro de uma doação e salvando no banco de dados / criando getDonationByID

--- src/controllers/donationController.ts
+++ src/controllers/donationController.ts
@@ -2,7 +2,8 @@ import { Request, Response } from 'express';
 import { DonationService } from '../services/donationServices';
 import { Password } from '../securities/password';
 import { Token } from '../securities/token';
-import { Donation } from '../models/donationModel';
+import { Donation, DonationInsert } from '../models/donationModel';
+import { validate } from 'class-validator';
 
 export class DonationController {
   static async getDonation(req: Request, res: Response): Promise<Response> {
@@ -14,7 +15,34 @@ export class DonationController {
   }
 
   static async createDonation(req: Request, res: Response): Promise<Response> {
-    return res.status(200).json({ access_token: "token" });
+    let payload = new DonationInsert(req.body);
+    const user_id = req.user_id;
+
+    const errors = await validate(payload);
+
+    if (errors.length > 0) {
+      const firstError = errors[0];
+      const errorMessage = firstError.constraints ? Object.values(firstError.constraints)[0] : "Parâmetros inválidos e/ou incompletos";
+      return res.status(400).json({ msg: errorMessage });
+    }
+
+    const { createdDonationID, error: createDonationError } = await DonationService.createDonation(payload, user_id);
+    if (createDonationError) {
+      return res.status(500).json({ msg: createDonationError });
+    }
+    if (!createdDonationID || createdDonationID === "") {
+      return res.status(404).json({ msg: 'Nenhum dado encontrado' });
+    }
+
+    const { donation, error: getDonationError } = await DonationService.getDonationById(createdDonationID);
+    if (getDonationError) {
+      return res.status(500).json({ msg: getDonationError });
+    }
+    if (!donation) {
+      return res.status(404).json({ msg: 'Nenhum dado encontrado' });
+    }
+
+    return res.status(201).json(donation);
   }
 
   static async updateDonation(req: Request, res: Response): Promise<Response> {
--- src/services/donationServices.ts
+++ src/services/donationServices.ts
@@ -1,3 +1,46 @@
-export class DonationService {
+import { Donation } from "../models/donationModel";
+import { query } from "../utils/database";
 
+export class DonationService {
+    static async getDonations(): Promise<{ donations: Donation[] | null, error: string | null }>{
+        return { donations: null, error: 'Erro interno do servidor' };
+    }
+
+    static async getDonationById(donation_id: string): Promise<{ donation: Donation | null, error: string | null }>{
+        try {
+            const result = await query('SELECT * FROM donations WHERE id = $1', [donation_id]);
+            if (result && result.rows && result.rows.length > 0) {
+              const donation = result.rows[0];
+              return { donation, error: null };
+            }
+            return { donation: null, error: null };
+          } catch (error) {
+            console.error('Erro ao buscar usuário por ID:', error);
+            return { donation: null, error: 'Erro interno do servidor' };
+          }
+    }
+
+    static async createDonation(data: any, user_id: string): Promise<{ createdDonationID: string | null; error: string | null }> {
+        try {
+            const { name, description, goal, url_image, deadline, state, category } = data;
+      
+            const result = await query('INSERT INTO donations (name, description, goal, url_image, deadline, state, category, user_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id', [name, description, goal, url_image, deadline, state, category, user_id]);
+            if (result && result.rows && result.rows.length > 0 && result.rows[0].id) {
+              return { createdDonationID: result.rows[0].id, error: null };
+            }
+      
+            return { createdDonationID: null, error: null };
+          } catch (error) {
+            console.error('Erro ao criar usuário:', error);
+            return { createdDonationID: '', error: 'Erro interno do servidor' };
+          }
+    }
+
+    static async updateDonation(donation_id: string, updatedDonationData: any): Promise<{ updatedDonation: any | null; error: string | null }> {
+        return { updatedDonation: null, error: 'Erro interno do servidor'};
+    }
+
+    static async deleteDonation(donation_id: string): Promise<{ deletedDonation: any | null; error: string | null }> {
+        return { deletedDonation: null, error: 'Erro interno do servidor'};
+    }
 }
