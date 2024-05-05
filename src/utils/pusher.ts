6d3fe8912e5164d319a2549d1b865ee0172a3077
ViniciusGR797 <loritoamarelo@outlook.com>
2024-05-05 17:20:10 -0300

    🥓 feat: Add config do pusher

--- src/utils/pusher.ts
+++ src/utils/pusher.ts
@@ -1,11 +1,12 @@
 import Pusher from 'pusher';
+import config from '../config';
 
 const pusher = new Pusher({
-  appId: 'YOUR_APP_ID',
-  key: 'YOUR_APP_KEY',
-  secret: 'YOUR_APP_SECRET',
-  cluster: 'YOUR_APP_CLUSTER',
-  useTLS: true
+  appId: config.pusher.appId,
+  key: config.pusher.key,
+  secret: config.pusher.secret,
+  cluster: config.pusher.cluster,
+  useTLS: config.pusher.useTLS
 });
 
 export default pusher;
